61dcd4de1ce7d358f42548bfecefd68722ef498f
Abbas Askari <abbasaskari797@gmail.com>
2023-11-24 11:11:49 +0500

    removed slash

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const io = new Server(server, {
   cors: {
     // origin: "http://localhost:5173",
     // https://chat-app-abbas.netlify.app/
-    origin: ["https://chat-app-abbas.netlify.app/", "http://localhost:5173"],
+    origin: ["https://chat-app-abbas.netlify.app", "http://localhost:5173"],
   },
 });
 
